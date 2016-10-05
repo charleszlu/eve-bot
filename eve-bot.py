9f7513aad3644f11bfee6ee7e2d7f8195931bbd3
Charles Lu <charles@Mumbletest.charleslu.net>
2016-10-05 21:45:42 +1100

    Updated to mumble version 1.2.17

--- eve-bot.py
+++ eve-bot.py
@@ -416,10 +416,10 @@ class mumbleConnection(threading.Thread):
             return
         print time.strftime("%a, %d %b %Y %H:%M:%S +0000"),self.threadName,"connected to server"
         pbMess = Mumble_pb2.Version()
-        pbMess.release="1.2.16"
+        pbMess.release="1.2.17"
         #pbMess.version=66048
-		#If version 1.2.7 then use bit masked version (1 << 16) + (2 << 8) + 7 => 66055
-        pbMess.version=66064
+		#If version 1.2.17 then use bit masked version (1 << 16) + (2 << 8) + 17 => 65536 + 512 + 17 = 66065
+        pbMess.version=66065
         pbMess.os=platform.system()
         pbMess.os_version="evebot1.0.2"
 
